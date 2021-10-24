e6a66723b5b6fda27f2ce4d11d68afc50ee042a0
Gnik Droy <gnikdroy@gmail.com>
2021-10-24 17:43:17 +0545

    document cardgrid

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
-/* <CardGrid card={Card} data={[]} key_fn={} /> */
+/**
+ * Grid like display of elements.
+ *
+ * @component
+ */
 function CardGrid(props) {
     return (
         <div className="container">
@@ -10,6 +15,25 @@ function CardGrid(props) {
         </div>
     );
 }
+CardGrid.propTypes = {
+    /**
+     * The Component to use to render each card.
+     */
+    card: PropTypes.elementType.isRequired,
+    /**
+     * The list of elements to display
+     */
+    data: PropTypes.array,
+    /**
+     * A function used to extract key from every item.
+     * has the form `(item) => key`
+     */
+    key_fn: PropTypes.func.isRequired
+}
+CardGrid.defaultProps = { 
+    data: [],
+}
+
 
 export default CardGrid
 

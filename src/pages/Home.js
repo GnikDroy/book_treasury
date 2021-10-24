ab6ca26bcf2477ab2f371538cae9eb2dc46482c9
Gnik Droy <gnikdroy@gmail.com>
2021-10-24 18:32:27 +0545

    document home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import BookCatalogue from '../components/BookCatalogue';
 
+/**
+ * Component to display contents of the homepage.
+ *
+ * @component
+ */
 function Home() {
   return <BookCatalogue query={{}} />;
 }

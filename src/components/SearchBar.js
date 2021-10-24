2d298d79819e7d5d1420118948467eb461358822
Gnik Droy <gnikdroy@gmail.com>
2021-10-24 12:54:17 +0545

    search on enter in search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,9 +7,25 @@ function SearchBar(props) {
     const input = React.useRef();
     return (
         <div className="input-group mb-3">
-            <input ref={input} type="text" className="form-control" placeholder={props.placeholder} aria-label={props.placeholder} aria-describedby="basic-addon2" />
+            <input ref={input}
+                type="text"
+                className="form-control"
+                placeholder={props.placeholder}
+                aria-label={props.placeholder}
+                aria-describedby="basic-addon2"
+                onKeyUp={(event) => {
+                    if (event.key === "Enter") {
+                        props.action(input.current);
+                    }
+                }}
+            />
             <div className="input-group-append">
-                <button className="btn-lg btn-success" type="button" onClick={() => props.action(input.current)}>{props.btn_label}</button>
+                <button
+                    className="btn-lg btn-success"
+                    type="button"
+                    onClick={() => props.action(input.current)}>
+                    {props.btn_label}
+                </button>
             </div>
         </div>
     );

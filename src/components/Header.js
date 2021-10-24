5891fb9ff081d45a7a43aba3f9cd3de97076f1d6
Gnik Droy <gnikdroy@gmail.com>
2021-10-24 10:25:46 +0545

    remove bootstrap package and use a CDN instead

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,23 +6,20 @@ import SiteThemeSelector from './SiteThemeSelector';
 function Header() {
     const history = useHistory();
     return (
-        <div className="container-fluid p-relative">
-            <div style={{ position: "absolute", top: "20px", right: "30px" }}>
+        <div className="fade-slide-top container py-5 position-relative">
+            <div style={{ position: "absolute", top: "10px", right: "5px" }}>
                 <SiteThemeSelector />
             </div>
-
-            <div className="fade-slide-top container my-5">
-                <div className="row align-items-center">
-                    <div className="col-md-8 p-2">
-                        <Link to="/" className="text-decoration-none" style={{ color: "inherit" }}>
-                            <h1 className="display-1">
-                                <i className="fas fa-book-open"></i>&nbsp;Book Treasury
-                            </h1>
-                        </Link>
-                    </div>
-                    <div className="col-md-4 p-2">
-                        <SearchBar placeholder="Book Title" btn_label="Search" action={input_elem => history.push("/search/" + input_elem.value)} />
-                    </div>
+            <div className="row align-items-center">
+                <div className="col-md-8 p-2">
+                    <Link to="/" className="text-decoration-none" style={{ color: "inherit" }}>
+                        <h1 className="display-1">
+                            <i className="fas fa-book-open"></i>&nbsp;Book Treasury
+                        </h1>
+                    </Link>
+                </div>
+                <div className="col-md-4 p-2">
+                    <SearchBar placeholder="Book Title" btn_label="Search" action={input_elem => history.push("/search/" + input_elem.value)} />
                 </div>
             </div>
         </div>

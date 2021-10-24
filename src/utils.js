4e1645ebe55e778162857de1db6f5a46bc19b2b6
Gnik Droy <gnikdroy@gmail.com>
2021-10-24 18:37:35 +0545

    document utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+/**
+ * Fetch the theme preference of the user
+ * @returns {string} - The theme preference as either "dark" or "light".
+ */
 export function getThemePreference() {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         return "dark";

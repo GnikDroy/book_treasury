06bad0972958b460785689af2f056c44977a2afc
Gnik Droy <gnikdroy@gmail.com>
2021-10-24 18:22:18 +0545

    document book api

--- src/api/BookApi.js
+++ src/api/BookApi.js
@@ -1,22 +1,51 @@
 import axios from 'axios';
 
-const api_base = "https://gnikdroy.pythonanywhere.com/api"
+/**
+ * API root location
+
+ * @type {object}
+ */
+export const api_base = "https://gnikdroy.pythonanywhere.com/api"
 
 
+/**
+ * Fetch a book from the api
+ * @param {Number} book_id - The book id.
+ * @param {object} query - The query object
+ * @returns {Promise} - The promise of the response as returned by axios.
+ */
 export function fetch_book(book_id, query = {}) {
     const url = api_base + "/book/" + book_id;
     return axios.get(url, {params: query });
 }
 
+/**
+ * Fetch books from the api
+ * @param {object} query - The query object
+ * @returns {Promise} - The promise of the response as returned by axios.
+ */
 export function fetch_books(query) {
     const url = api_base + "/book";
     return axios.get(url, {params: query });
 }
 
+/**
+ * Parse books returned from the api
+ * Extracts the cover and author and adds them as attributes to the given object.
+ * 
+ * @param {object} book - The book object from the api.
+ * @returns {object} - The modified book object.
+ */
 export function parse_book(book) {
     return { ...book, cover: extract_cover(book), author: extract_author(book) };
 }
 
+/**
+ * Extract author from the book
+ * 
+ * @param {object} book - The book object from the api.
+ * @returns {any} - Either the agent object or null.
+ */
 function extract_author(book) {
     for (const agent of book.agents) {
         if (agent.type === "Author") {
@@ -26,6 +55,13 @@ function extract_author(book) {
     return null;
 }
 
+
+/**
+ * Extract epub resource from the book
+ * 
+ * @param {object} book - The book object from the api.
+ * @returns {any} - Either the resource object or null.
+ */
 export function extract_epub(book) {
     if (book.resources == null) { return null; }
     for (const resource of book.resources) {
@@ -41,6 +77,12 @@ export function extract_epub(book) {
     return null;
 }
 
+/**
+ * Extract html resource from the book
+ * 
+ * @param {object} book - The book object from the api.
+ * @returns {any} - Either the resource object or null.
+ */
 export function extract_html(book) {
     if (book.resources == null) { return null; }
     for (const resource of book.resources) {
@@ -51,12 +93,38 @@ export function extract_html(book) {
     return null;
 }
 
+/**
+ * Extract cover resource from the book.
+ * 
+ * @param {object} book - The book object from the api.
+ * @returns {any} - Either the resource object or null.
+ */
 function extract_cover(book) {
     for (const resource of book.resources) {
-        if (resource.type === "image/jpeg" && resource.uri.endsWith(".cover.medium.jpg")) {
+        if (resource.type === "image/jpeg" && (
+            resource.uri.endsWith(".cover.medium.jpg") ||
+            resource.uri.endsWith(".cover.medium.jpeg")
+            )) {
+            return resource;
+        }
+    }
+    for (const resource of book.resources) {
+        if (resource.type === "image/jpeg" && ( 
+            resource.uri.endsWith(".cover.small.jpg") ||
+            resource.uri.endsWith(".cover.small.jpeg")
+            )) {
+            return resource;
+        }
+    }
+    for (const resource of book.resources) {
+        if (resource.type === "image/png" && resource.uri.endsWith(".cover.medium.png")) {
+            return resource;
+        }
+    }
+    for (const resource of book.resources) {
+        if (resource.type === "image/png" && resource.uri.endsWith(".cover.small.png")) {
             return resource;
         }
     }
     return null;
-
 }

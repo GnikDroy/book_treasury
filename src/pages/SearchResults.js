9a187884378d7902c7e6016099a79907d6979676
Gnik Droy <gnikdroy@gmail.com>
2021-10-24 18:32:48 +0545

    document search results page

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -2,6 +2,11 @@ import React from 'react';
 import BookCatalogue from "../components/BookCatalogue";
 import { useParams } from 'react-router-dom';
 
+/**
+ * Component to display contents of the search results page.
+ *
+ * @component
+ */
 function SearchResults(props) {
     let { q } = useParams();
     return <BookCatalogue query={{ search: q }} />;
